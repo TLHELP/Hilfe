70a98491f14bc04805a67819aa55d6b87612dc5f
marcus <marcus@DESKTOP-SELO19E>
2019-11-04 14:12:35 +0100

    da

--- tktltle/Program.cs
+++ tktltle/Program.cs
@@ -6,7 +6,7 @@ namespace tktltle
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello rld!");
+            Console.WriteLine("Hello rwld!");
         }
     }
 }
